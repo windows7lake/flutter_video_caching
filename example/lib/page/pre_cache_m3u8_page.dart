68208e62b8386e85f16a8e1afa773b34e683d92b
LioLin <windows7lake@gmail.com>
2025-06-05 10:18:10 +0800

    feat: add func for parse HlsMasterPlaylist

--- example/lib/page/pre_cache_m3u8_page.dart
+++ example/lib/page/pre_cache_m3u8_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_video_caching/ext/file_ext.dart';
 import 'package:flutter_video_caching/ext/log_ext.dart';
+import 'package:flutter_video_caching/ext/uri_ext.dart';
 import 'package:flutter_video_caching/flutter_video_caching.dart';
 
 class PreCacheM3u8Page extends StatefulWidget {
@@ -13,114 +14,180 @@ class PreCacheM3u8Page extends StatefulWidget {
 }
 
 class _PreCacheM3u8PageState extends State<PreCacheM3u8Page> {
-  final ValueNotifier<List<Map>> _segmentProgress = ValueNotifier([]);
-
+  final ValueNotifier<int> _step = ValueNotifier<int>(0);
   final ValueNotifier<String> _hlsKey = ValueNotifier('');
+  final List<Map> _segmentProgress = <Map>[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pre-cache M3u8'),
-        actions: [
-          ValueListenableBuilder(
-              valueListenable: _hlsKey,
-              builder: (ctx, hlsKey, _) {
-                return hlsKey.isEmpty
-                    ? SizedBox()
-                    : IconButton(
-                        onPressed: () async {
-                          String dirPath =
-                              '${FileExt.cacheRootPath}/videos/$hlsKey';
-                          await LruCacheSingleton().storageClearByDirPath(dirPath);
-                          _segmentProgress.value = [];
-                        },
-                        icon: Icon(
-                          Icons.delete_forever,
-                          color: Colors.red,
-                          size: 24,
-                        ),
-                      );
-              }),
-        ],
-      ),
+      appBar: appBar(),
       body: ValueListenableBuilder(
-          valueListenable: _segmentProgress,
-          builder: (ctx, segmentProgress, _) {
-            return segmentProgress.isNotEmpty
-                ? SingleChildScrollView(
-                    child: Column(
-                      children: segmentProgress.map((Map map) {
-                        return Card(
-                          margin:
-                              EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: map.entries.map((entry) {
-                                return Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 5.0),
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        flex: 2,
-                                        child: Text(
-                                          '${entry.key}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 3,
-                                        child: Text('${entry.value}'),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              }).toList(),
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  )
-                : Center(
-                    child: TextButton(
-                      onPressed: () {
-                        VideoCaching.precache(
-                          'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
-                          cacheSegments: 999,
-                          progressListen: true,
-                        ).then((streamController) {
-                          if (streamController == null) return;
-                          StreamSubscription? subscription;
-                          subscription =
-                              streamController.stream.listen((value) {
-                            logD(
-                                'segment download progress: $value, ${value['hls_key']}');
-                            _hlsKey.value = value['hls_key'];
-                            final currentList =
-                                List<Map<dynamic, dynamic>>.from(
-                                    _segmentProgress.value);
-                            currentList.add(value);
-                            _segmentProgress.value = currentList;
+        valueListenable: _step,
+        builder: (ctx, step, _) {
+          logD("step: $step");
+          if (step == 0) {
+            return body();
+          } else if (step == 1) {
+            return hlsMasterPlaylist();
+          }
+          return segmentList();
+        },
+      ),
+    );
+  }
 
-                            if (value['progress'] == 1) {
-                              subscription?.cancel();
-                              streamController.close();
-                              logD('close listener');
-                              setState(() {});
-                              return;
-                            }
-                          });
-                        });
-                      },
-                      child: Text('Start Pre-cache'),
+  PreferredSizeWidget appBar() {
+    return AppBar(
+      title: const Text('Pre-cache M3u8'),
+      actions: [
+        ValueListenableBuilder(
+          valueListenable: _hlsKey,
+          builder: (ctx, hlsKey, _) {
+            return hlsKey.isEmpty
+                ? SizedBox()
+                : IconButton(
+                    onPressed: () async {
+                      String dirPath =
+                          '${FileExt.cacheRootPath}/videos/$hlsKey';
+                      await LruCacheSingleton().storageClearByDirPath(dirPath);
+                      _segmentProgress.clear();
+                      _step.value = 0;
+                    },
+                    icon: Icon(
+                      Icons.delete_forever,
+                      color: Colors.red,
+                      size: 24,
                     ),
                   );
-          }),
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget hlsMasterPlaylist() {
+    return SingleChildScrollView(
+      child: Column(
+        children: _segmentProgress.map((Map map) {
+          return GestureDetector(
+            onTap: () async {
+              _step.value = 2;
+              _segmentProgress.clear();
+              VideoCaching.precache(
+                '${map['url']}',
+                cacheSegments: 999,
+                progressListen: true,
+              ).then((streamController) {
+                _segmentProgress.clear();
+                if (streamController == null) return;
+                StreamSubscription? subscription;
+                subscription = streamController.stream.listen((value) {
+                  logD('segment download progress: $value');
+                  _hlsKey.value = value['hls_key'];
+                  _segmentProgress.add(value);
+                  setState(() {});
+
+                  if (value['progress'] == 1) {
+                    subscription?.cancel();
+                    streamController.close();
+                    logD('close listener');
+                    return;
+                  }
+                });
+              });
+            },
+            child: Card(
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: map.entries.map((entry) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              '${entry.key}',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Text('${entry.value}'),
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget segmentList() {
+    return SingleChildScrollView(
+      child: Column(
+        children: _segmentProgress.map((Map map) {
+          return Card(
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: map.entries.map((entry) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Text(
+                            '${entry.key}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Text('${entry.value}'),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget body() {
+    return Center(
+      child: TextButton(
+        onPressed: () async {
+          _step.value = 1;
+          Uri uri = Uri.parse(
+              'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8');
+          HlsMasterPlaylist? playlist =
+              await VideoCaching.parseHlsMasterPlaylist(uri.toString());
+          List<Map>? resolutions = playlist?.mediaPlaylistUrls
+              .map((e) => {"url": '${uri.pathPrefix()}${e?.path}'})
+              .toList();
+          _segmentProgress.clear();
+          _segmentProgress.addAll(resolutions ?? []);
+          setState(() {});
+        },
+        child: Text('Start Pre-cache'),
+      ),
     );
   }
 }
--- lib/parser/video_caching.dart
+++ lib/parser/video_caching.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter_hls_parser/flutter_hls_parser.dart';
+import 'package:flutter_video_caching/ext/uri_ext.dart';
+
+import 'url_parser.dart';
 import 'url_parser_factory.dart';
+import 'url_parser_m3u8.dart';
 
 class VideoCaching {
   /// Parse the URL and cache the video
@@ -26,5 +31,19 @@ class VideoCaching {
   }) {
     return UrlParserFactory.createParser(Uri.parse(url))
         .precache(url, cacheSegments, downloadNow, progressListen);
+  }
+
+  /// Parse the HLS master playlist from the given URL.
+  ///
+  /// [url]: The URL of the HLS master playlist.
+  ///
+  /// Returns an instance of [HlsMasterPlaylist] if successful, otherwise returns null.
+  static Future<HlsMasterPlaylist?> parseHlsMasterPlaylist(String url) async {
+    Uri uri = Uri.parse(url);
+    UrlParser parser = UrlParserFactory.createParser(uri);
+    if (parser is! UrlParserM3U8) return null;
+    HlsPlaylist? playlist =
+        await parser.parsePlaylist(uri, hlsKey: uri.generateMd5);
+    return playlist is HlsMasterPlaylist ? playlist : null;
   }
 }
