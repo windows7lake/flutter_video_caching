15f82c8e290240beb3ae40aeacfb638b8cdaa547
LioLin <windows7lake@gmail.com>
2025-09-02 17:19:51 +0800

    fix: dart format

--- lib/cache/lru_cache_singleton.dart
+++ lib/cache/lru_cache_singleton.dart
@@ -130,7 +130,7 @@ class LruCacheSingleton {
     await _storageInit();
     return _storageCache.size.toMemorySize;
   }
-  
+
   /// Returns the size of the disk cache.
   Future<int> storageSizeInBytes() async {
     await _storageInit();
